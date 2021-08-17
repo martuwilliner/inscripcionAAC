3b044a6fc55affb73f1248bfc05030ef3619eb0f
martuwilliner <williner.martin@gmail.com>
2021-08-17 12:35:50 -0300

    avances

--- main.js
+++ main.js
@@ -1,10 +1,56 @@
-const form = document.querySelector("#form-contact");
+// Selection
+const btnSteps = Array.from(document.querySelectorAll("button[data-step]"))
+const steps = Array.from(document.querySelectorAll(".step"))
+const present = Array.from(document.querySelectorAll('input[name="present"]'))
+const popUps = Array.from(document.querySelectorAll('.popUp'))
+const btnPop = Array.from(document.querySelectorAll('button[data-pop]'))
+// Functions
+
+const showStep = (id) => {
+    steps.forEach(step => step.classList.remove("active"));
+    const step = steps.find(step => step.getAttribute("id") == id)
+    if(!step.classList.contains('active')){
+        step.classList.add('active');
+    }
+    console.clear();
+    console.log(step);
+}
+
+const showPopUp = (id) => {
+    const pop = popUps.find(pop => pop.getAttribute("id") == id);
+    if(!pop.classList.contains('active')){
+        popUps.forEach(pop => pop.classList.remove("active"));
+        pop.classList.add('active');
+    }else{
+        popUps.forEach(pop => pop.classList.remove("active"));
+    }
+    
+    console.clear();
+    console.log(pop);
+}
 
 
-form.addEventListener("submit",(e) => {
+btnSteps.forEach(btn => btn.addEventListener('click',(e)=>{
     e.preventDefault();
-    const data = new FormData(e.target);
-    console.log(data);
-    //fetch("./email.php").then(res => res.json()).then(data => console.log(data) )
-   
-})
+    const target = e.target;
+    const step = target.dataset.step
+    showStep(step)
+}))
+
+present.forEach(input => input.addEventListener('input',(e)=>{
+    e.preventDefault()
+    const target = e.target;
+    const pop = target.dataset.pop;
+    showPopUp(pop);
+}))
+
+btnPop.forEach(btn => btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const target = e.target;
+    let pop = target.dataset.pop;
+    if(pop == undefined){
+        pop = target.parentElement.dataset.pop
+    }
+    showPopUp(pop);
+    console.clear();
+}))
